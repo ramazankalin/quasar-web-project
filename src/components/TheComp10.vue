753fc65f4810476de927d8d0f810ba66508e6668
özgenur bilican <ozgenurblcn@gmail.com>
2023-12-12 23:51:55 +0300

    TheComp10
    blog sayfası

---
+++ src/components/TheComp10.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+    <div  style="width :700px;font-family: 'Verona-Regular',sans-serif;text-align: end;text-decoration: underline;font-weight: bold;margin-bottom: 30px;margin-top: 50px;font-size: 20px">Kahve Çeşitleri Blog</div>
+    <div class="column" style="height: 800px; max-height: 100%">
+      <div class="col-9" style=" width: 1200px;" >
+        <q-btn style="width: 1200px; height: 800px;border: none;background-color:white;margin-left: 40px;"><img src="https://static.ticimax.cloud/2571/Uploads/Blog/Ceviz-Faydalari-Nelerdir-a79b.png" ></q-btn>
+      </div>
+      <div class="col-5">
+      <h6>Kategoriler</h6>
+      <q-link style="cursor: pointer;color: grey;"> <li> kahve çeşitleri</li><br></q-link>
+      <q-link style="cursor: pointer;color: grey;"><li>Bayram Şekeri</li><br></q-link>
+      <q-link style="cursor: pointer;color: grey;"><li> kuruyemiş çeşitleri</li><br></q-link>
+      <q-link style="cursor: pointer;color: grey;"><li>Kolonya</li><br></q-link>
+      <h6>Son Blog Yazıları</h6>
+      <q-link><img src="https://static.ticimax.cloud/2571/Uploads/Blog/Ceviz-Faydalari-Nelerdir-a79b.png" style="width: 300px;"></q-link>
+      <q-link style="cursor: pointer;color: grey;" > <li> Ceviz Faydaları Nelerdir?</li><br></q-link>
+      </div>
+      </div>
+    </div>
+</template>
+<style>
+
+.col-9 :hover{
+background-color: gray;
+}
+.col-5{
+  text-decoration: underline;
+  font-family: 'Verona-Regular',sans-serif
+}
+
+</style>
